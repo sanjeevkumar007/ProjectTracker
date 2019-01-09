9d7354ba010ac8e8a2158bc8ffa6813f2980521d
sanjeevkumar007 <san.ank007@gmail.com>
2019-01-10 00:34:44 +0530

    Added Ilogger

---
+++ ProjectTracker.APILogger/Log.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectTracker.APILogger
+{
+    public sealed class Log : ILogService
+    {
+        private static readonly Lazy<Log> instance = new Lazy<Log>();
+
+        private Log()
+        {
+
+        }
+
+        public static Log GetInstance
+        {
+            get
+            {
+                return instance.Value;
+            }
+        }
+
+        public async void LogExceptionAsync(string message)
+        {
+            string fileName = string.Format("{0}_{1}.log", "Exception", DateTime.Now.ToString());
+            string logPath = string.Format(@"{0}\{1}", AppDomain.CurrentDomain.BaseDirectory, fileName);
+            StringBuilder builder = new StringBuilder();
+            builder.AppendLine("-------------------------------------------------");
+            builder.AppendLine(DateTime.Now.ToString());
+            builder.AppendLine(message);
+            using (StreamWriter writer = new StreamWriter(logPath, true))
+            {
+                await writer.WriteAsync(builder.ToString());
+                await writer.FlushAsync();
+            }
+        }
+    }
+}
---
+++ ProjectTracker.UI/Controllers/BaseController.cs
@@ -0,0 +1,29 @@
+﻿using ProjectTracker.APILogger;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.ExceptionHandling;
+
+namespace ProjectTracker.UI.Controllers
+{
+    public class BaseController : ApiController,IExceptionHandler
+    {
+        private readonly ILogService _log;
+
+        //public async Task HandleAsync(ExceptionHandlerContext context, CancellationToken cancellationToken)
+        //{
+        //    _log.LogExceptionAsync(context.Exception.Message);
+        //    return Task.FromResult(0);
+        //}
+
+        public Task LogAsync(ExceptionLoggerContext context, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
