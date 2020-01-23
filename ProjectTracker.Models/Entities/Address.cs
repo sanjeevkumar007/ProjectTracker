fae026d0fcae666a68002eacbbb02f9c1ad960ad
703192856 <703192856@703192856L>
2020-01-23 23:25:22 +0530

    Added Address class

---
+++ ProjectTracker.Models/Entities/Address.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectTracker.Models.Entities
+{
+    public class Address
+    {
+        public int AdddressId { get; set; }
+        public string AddressName { get; set; }
+    }
+}
